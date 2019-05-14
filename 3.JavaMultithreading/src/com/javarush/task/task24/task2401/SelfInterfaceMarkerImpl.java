4663933da8812e9c6cf7d9532563d7e350439935
leovalter83@gmail.com <NqnDffVc4fyq>
2019-05-14 23:30:19 +0300

    Initial commit

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void oneMethod() {
+        System.out.println("Method 1");
+    }
+
+    public void twoMethod() {
+        System.out.println("Method 2");
+    }
+
+}
