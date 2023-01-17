f66239940f864aa72cb597356430cdb5b0d7a34f
leovalter <leovalter83@gmail.com>
2023-01-17 21:03:59 +0300

    Task2028

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /* 
 Построй дерево(1)
@@ -10,7 +13,7 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     Entry<String> root;
 
-    private List<Entry> list = new ArrayList<>();
+    private final List<Entry<String>> list = new ArrayList<>();
 
     static class Entry<T> implements Serializable {
         String elementName;
@@ -29,9 +32,12 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
         }
 
         public boolean isAvailableToAddChildren() {
-            return availableToAddLeftChildren || availableToAddRightChildren;
+            return (availableToAddLeftChildren || availableToAddRightChildren);
         }
 
+        public boolean noChildren() {
+            return leftChild == null & rightChild == null;
+        }
     }
 
     public CustomTree() {
@@ -39,26 +45,17 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
         list.add(0, root);
     }
 
-    public String getParent(String s) {
-        String parent = null;
-        for (Entry e: list) {
-            if (e.elementName.equals(s)) parent = e.parent.elementName;
-        }
-        return parent;
-    }
-
     @Override
     public boolean add(String elementName) {
-        Entry current;
-        Entry newEntry = new Entry(elementName);
-        for (int i=0; i<list.size(); i++) {
+        Entry<String> current;
+        Entry<String> newEntry = new Entry<>(elementName);
+        for (int i = 0; i < list.size(); i++) {
             current = list.get(i);
             if (current.isAvailableToAddChildren()) {
-                if(current.leftChild==null) {
+                if (current.leftChild == null) {
                     current.leftChild = newEntry;
-                }
-                else current.rightChild = newEntry;
-                newEntry.parent=current;
+                } else current.rightChild = newEntry;
+                newEntry.parent = current;
                 current.checkChildren();
                 list.add(newEntry);
                 return true;
@@ -67,21 +64,112 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
         return false;
     }
 
-    @Override
-    public int size() {
-        int count=0;
-        for(Entry e: this.list) {
-            if (e!=null) {
-                count++;
+    public boolean remove(Object o) {
+        String elementName;
+        if (!(o instanceof String)) {
+            throw new UnsupportedOperationException();
+        } else {
+            elementName = String.valueOf(o);
+        }
+        List<Entry<String>> entryForRemove = new ArrayList<>();
+        for (Entry<String> entry : list) {
+            String entryElementName = entry.elementName;
+            if (elementName.equals(entryElementName)) {
+                if (entry.parent.leftChild.elementName.equals(entryElementName)) {
+                    entry.parent.availableToAddLeftChildren = true;
+                } else {
+                    entry.parent.availableToAddRightChildren = true;
+                }
+                entryForRemove = getChildren(entry);
             }
         }
-        return count-1;
+
+//        System.out.println(entryForRemove);
+
+        if (entryForRemove.size() > 0) {
+            for (Entry<String> entry : entryForRemove) {
+                list.remove(entry);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private List<Entry<String>> getChildren(Entry<String> entry) {
+        List<Entry<String>> result = new ArrayList<>();
+        List<Entry<String>> leftChild = new ArrayList<>();
+        List<Entry<String>> rightChild = new ArrayList<>();
+
+        if (entry.noChildren()) {
+            result.add(entry);
+            return result;
+        } else if (entry.leftChild == null) {
+            leftChild.add(entry);
+            rightChild = getChildren(entry.rightChild);
+        } else if (entry.rightChild == null) {
+            leftChild = getChildren((entry.leftChild));
+            rightChild.add(entry);
+        } else {
+            leftChild = getChildren((entry.leftChild));
+            rightChild = getChildren(entry.rightChild);
+            leftChild.add(entry);
+        }
+
+        result = joinLists(leftChild, rightChild);
+
+        return result;
     }
 
     @Override
     public String get(int i) {
         throw new UnsupportedOperationException();
     }
+
+    public String getParent(String s) {
+        return list.stream()
+                .filter(entry -> s.equals(entry.elementName))
+                .findFirst()
+                .map(entry -> String.valueOf(entry.parent.elementName))
+                .orElse(null);
+    }
+
+    @Override
+    public int size() {
+        int count = 0;
+        for (Entry<String> e : this.list) {
+            if (e != null) {
+                count++;
+            }
+        }
+        return count - 1;
+    }
+
+    public static <T> ArrayList<T> joinLists(
+            final List<? extends T> listA,
+            final List<? extends T> listB) {
+
+        boolean aEmpty = (listA == null) || listA.isEmpty();
+        boolean bEmpty = (listB == null) || listB.isEmpty();
+        //побитное И!
+        if (aEmpty & bEmpty) {
+            // оба пустые — отдаем новый пустой список
+            return new ArrayList<>();
+        } else if (aEmpty) {
+            // один пустой — отдаем копию другого, содержащую все его элементы
+            return new ArrayList<>(listB);
+        } else if (bEmpty) {
+            return new ArrayList<>(listA);
+        } else {
+            // оба непустые — объединяем
+            ArrayList<T> result = new ArrayList<>(
+                    listA.size() + listB.size());
+            result.addAll(listA);
+            result.addAll(listB);
+            return result;
+        }
+    }
+
     @Override
     public String set(int i, String s) {
         throw new UnsupportedOperationException();
@@ -110,5 +198,10 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     @Override
     public boolean addAll(int i, Collection<? extends String> collection) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
